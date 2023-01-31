93f8668179961768f6fecc7e4a610c91c48e6ca0
Luiz Dorça <luizdorca@gmail.com>
2023-01-30 21:33:52 -0300

    Mod-04. Atividade. Início.

---
+++ src/main/java/br/com/mentorama/Mod04Atividade/Mod04AtividadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.Mod04Atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod04AtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod04AtividadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mentorama/Mod04Atividade/Mod04AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mentorama.Mod04Atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod04AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
