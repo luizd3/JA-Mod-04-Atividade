cdcf784cffe2b69f12377df7784b018e805d98a1
Luiz Dorça <luizdorca@gmail.com>
2023-02-01 00:52:04 -0300

    Refatorado métodos de Service para Repository

---
+++ src/main/java/br/com/mentorama/Mod04Atividade/repositories/FilmesRepository.java
@@ -0,0 +1,47 @@
+package br.com.mentorama.Mod04Atividade.repositories;
+
+import br.com.mentorama.Mod04Atividade.exceptions.FilmeNaoEncontradoException;
+import br.com.mentorama.Mod04Atividade.models.Filme;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FilmesRepository {
+
+    private final List<Filme> filmes;
+
+    public FilmesRepository() {
+        this.filmes = new ArrayList<>();
+    }
+
+    public List<Filme> findAll() {
+        return filmes;
+    }
+
+    public void add(Filme filme) {
+        filme.setId(UUID.randomUUID());
+        this.filmes.add(filme);
+    }
+
+    public void update(Filme filme) {
+        Filme filmeParaAlterar = filmes.stream()
+                .filter(film -> film.getId().equals(filme.getId()))
+                .findFirst()
+                .orElseThrow(FilmeNaoEncontradoException::new);
+        filmeParaAlterar.setNome(filme.getNome());
+        filmeParaAlterar.setNomeDoDiretor(filme.getNomeDoDiretor());
+        filmeParaAlterar.setAno(filme.getAno());
+        filmeParaAlterar.setNota(filme.getNota());
+    }
+
+    public void delete(UUID id) {
+        boolean remove = filmes.removeIf(film -> film.getId().equals(id));
+        if (!remove) {
+            throw new FilmeNaoEncontradoException();
+        }
+    }
+
+}
--- src/main/java/br/com/mentorama/Mod04Atividade/services/FilmesService.java
+++ src/main/java/br/com/mentorama/Mod04Atividade/services/FilmesService.java
@@ -1,11 +1,11 @@
 package br.com.mentorama.Mod04Atividade.services;
 
-import br.com.mentorama.Mod04Atividade.exceptions.FilmeNaoEncontradoException;
 import br.com.mentorama.Mod04Atividade.exceptions.NotaInvalidaException;
 import br.com.mentorama.Mod04Atividade.models.Filme;
+import br.com.mentorama.Mod04Atividade.repositories.FilmesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -13,41 +13,27 @@ import java.util.UUID;
 @Service
 public class FilmesService {
 
-    private final List<Filme> filmes;
-    private UUID id;
     private int[] notas = {1,2,3,4,5};
 
-    public FilmesService() {
-        this.filmes = new ArrayList<>();
-    }
+    @Autowired
+    private FilmesRepository filmesRepository;
 
     public List<Filme> findAll() {
-        return filmes;
+        return filmesRepository.findAll();
     }
 
     public void add(Filme filme) {
         validaNota(filme.getNota());
-        filme.setId(UUID.randomUUID());
-        this.filmes.add(filme);
+        this.filmesRepository.add(filme);
     }
 
     public void update(Filme filme) {
         validaNota(filme.getNota());
-        Filme filmeParaAlterar = filmes.stream()
-                .filter(film -> film.getId().equals(filme.getId()))
-                .findFirst()
-                .orElseThrow(FilmeNaoEncontradoException::new);
-        filmeParaAlterar.setNome(filme.getNome());
-        filmeParaAlterar.setNomeDoDiretor(filme.getNomeDoDiretor());
-        filmeParaAlterar.setAno(filme.getAno());
-        filmeParaAlterar.setNota(filme.getNota());
+        filmesRepository.update(filme);
     }
 
     public void delete(UUID id) {
-        boolean remove = filmes.removeIf(film -> film.getId().equals(id));
-        if (!remove) {
-            throw new FilmeNaoEncontradoException();
-        }
+        filmesRepository.delete(id);
     }
 
     private void validaNota(int notaNova) {
